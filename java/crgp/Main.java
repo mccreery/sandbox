8178c0ce79f27ceab2dffef0c13e7405cc7cb6cf
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-06-16 14:50:08 +0100

    Gradle wrapper and comments on main

--- java/crgp/Main.java
+++ java/crgp/Main.java
@@ -1,7 +1,14 @@
 public class Main {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
+        // Both setX and setY return A so no problems
         A a = new A().setX(5).setY(10);
-        B b = new B().setX(5).setY(10).setZ(15);
+
+        // setY still returns A so setZ cannot be called without a downcast
+        B b = ((B)new B().setX(5).setY(10)).setZ(15);
+
+        // Using covariant return types all methods return C
+        // but every method has to be overridden in C
         C c = new C().setX(5).setY(10).setZ(15);
     }
 }
