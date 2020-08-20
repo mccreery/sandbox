bc5d83095c72efcc8c516727166e65dc6418abf7
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-08-21 00:54:31 +0100

    Delete CapacityList, overengineering

--- java/textadventure/src/main/java/adventure/Main.java
+++ java/textadventure/src/main/java/adventure/Main.java
@@ -1,16 +1,16 @@
 package adventure;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import adventure.glot.Glot;
 import adventure.item.Item;
-import adventure.util.CapacityList;
 
 public class Main {
     public static void main(String[] arguments) {
         Glot textGlot = new Glot(ResourceBundle.getBundle("text"));
-        List<Stack<Item>> inventory = new CapacityList<>(20);
+        List<Stack<Item>> inventory = new ArrayList<>(20);
 
         inventory.add(new Stack<>(Items.FLINT, 3));
         inventory.add(new Stack<>(Items.UNLIT_TORCH, 10));
